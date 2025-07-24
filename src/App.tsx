ccc876ed1cab0e43ce1dae98aa63f4d9f478c262
Leonardo Lunario <leonardolunario@gmail.com>
2025-07-24 18:57:28 +0800

    fix: correct type imports, CI workflow, and minor update to title of index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 import { Button } from './components/ui/Button';
 import { Link } from './components/ui/Link';
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)'
+      }
+    },
   },
-  plugins: []
+  plugins: [],
 }
